2905dbb76ba10da79e2113b418f21a20563da809
fuco99 <phannguyentien1999@gmail.com>
2021-08-19 08:33:50 +0700

    complete project

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// truncateCardTitle
+window.addEventListener("load", function () {
+  truncateCardTitle();
+});
+
+function truncateCardTitle() {
+  var cardList = document.getElementsByClassName("card-title");
+  console.log(cardList);
+  for (var i = 0; i < cardList.length; i++) {
+    cardList[i].innerHTML;
+    var text = cardList[i].innerHTML;
+    var newText = truncateString(text,40);
+    cardList[i].innerHTML = newText;
+  }
+}
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+// SIDEBAR MINI
+var toggleBtn = document.querySelector('.sidebarMini__button');
+var sidebarMini = document.querySelector('.sidebarMini');
+var switchBtn = document.querySelector('#checkbox');
+
+toggleBtn.addEventListener('click',function(){
+  sidebarMini.classList.toggle('is-opened');
+});
+
+switchBtn.addEventListener('click',function(){
+  document.querySelector('body').classList.toggle('darkMode');
+});
+
+
